619ba17440d2156faa6e1edb0ebcd141afabb358
Paula Sturmer <paulasturmer@gmail.com>
2020-05-04 08:37:30 -0300

    Resolucao parcial

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    collection do
+      get :top
+    end
+
+    member do
+      get :chef
+    end
+
+    resorces :reviews, only: [:new, :create]
+  end
+  resources :reviews, only: [:destroy]
 end
